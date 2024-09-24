076ac8e536e3027d4ca9012e80c6d15f9f600cf8
Matias Giache <matiasgiache08@gmail.com>
2024-09-24 19:04:00 -0300

    Adding Api Gateway and Routes

---
+++ api-gateway-ms/src/main/java/Api/Gateway/ApiGatewayMsApplication.java
@@ -0,0 +1,13 @@
+package Api.Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayMsApplication.class, args);
+	}
+
+}
---
+++ api-gateway-ms/src/test/java/Api/Gateway/ApiGatewayMsApplicationTests.java
@@ -0,0 +1,13 @@
+package Api.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
